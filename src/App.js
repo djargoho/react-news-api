7586be6b65feb95024f0dbe093f17a75621d9ff2
DJArgoho <djargoho@gmail.com>
2019-12-03 00:33:40 +0900

    making new views

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React, { useState, useCallback } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
-function App() {
-  const [data, setData] = useState(null);
-
-  const onClick = useCallback(async () => {
-    console.log('CLICK');
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=084c3f807c5c460ea07bda98e5a0b7c8',
-      );
-      setData(response.data);
-    } catch (error) {
-      throw new Error(error);
-    }
-  }, []);
+const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
 
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
-    </div>
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/NewsList.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    //async를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=084c3f807c5c460ea07bda98e5a0b7c8',
+        );
+
+        setArticles(response.data.articles);
+      } catch (error) {
+        throw new Error(error);
+      }
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>Loading...</NewsListBlock>;
+  }
+
+  if (!articles) {
+    return null;
+  }
+
+  //article이 존재할 때
+  return (
+    <NewsListBlock>
+      {articles.map(article => {
+        return <NewsItem key={article.url} article={article} />;
+      })}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
